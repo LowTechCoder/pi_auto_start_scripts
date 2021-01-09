e8b4c75b2a65d089907dacebe27e5fe70266eeff
Matt Eller <meller@affariproject.com>
2021-01-08 19:08:03 -0500

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pi_auto_start_scripts
+```
+cp /media/pi/USB30FD/desktop.txt /home/pi/.config/autostart/.desktop
+```
---
+++ autostart.bash
@@ -0,0 +1,13 @@
+#auto backup script
+my_date=$(date +'%Y-%m-%d')
+cd /media/pi/USBDRIVE/rasp_pi_retroarch_conf_backups/
+mkdir -p "$my_date/system/"
+#only back up system once
+#rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/system" "$my_date/"
+rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/config" "$my_date/"
+rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/autoconfig" "$my_date/"
+rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/playlists" "$my_date/"
+rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/saves" "$my_date/"
+rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/states" "$my_date/"
+
+retroarch -f
