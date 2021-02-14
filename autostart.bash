5aa384bf8e461b367d072e824f31bcdda044c9c1
LowTechCoder <73705040+LowTechCoder@users.noreply.github.com>
2021-02-14 13:35:45 -0500

    Update autostart.bash

--- autostart.bash
+++ autostart.bash
@@ -1,13 +1,15 @@
 #auto backup script
 my_date=$(date +'%Y-%m-%d')
-cd /media/pi/USBDRIVE/rasp_pi_retroarch_conf_backups/
+cd /media/pi/RA-DATA/rasp_pi_retroarch_conf_backups/
 mkdir -p "$my_date/system/"
 #only back up system once
-#rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/system" "$my_date/"
+rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/system" "$my_date/"
 rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/config" "$my_date/"
 rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/autoconfig" "$my_date/"
 rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/playlists" "$my_date/"
 rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/saves" "$my_date/"
 rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/states" "$my_date/"
+rsync -r "/home/pi/snap/retroarch/427/.config/retroarch/retroarch.cfg" "$my_date/"
 
+#retroarch -f && sudo shutdown now
 retroarch -f && echo 'To prevent shutdown, close this window!'; sleep 5;  sudo shutdown now
