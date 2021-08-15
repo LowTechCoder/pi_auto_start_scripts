15887b43d5dfbebdf28f3f913098dda3c610536b
LowTechCoder <73705040+LowTechCoder@users.noreply.github.com>
2021-08-15 11:54:41 -0400

    Update autostart.bash

--- autostart.bash
+++ autostart.bash
@@ -1,15 +1,17 @@
 #auto backup script
-my_date=$(date +'%Y-%m-%d')
-cd /media/pi/RA-DATA/rasp_pi_retroarch_conf_backups/
-radir="/home/pi/.config/retroarch"
-mkdir -p "$my_date/system/"
-rsync -r "$radir/system" "$my_date/"
-rsync -r "$radir/config" "$my_date/"
-rsync -r "$radir/autoconfig" "$my_date/"
-rsync -r "$radir/playlists" "$my_date/"
-rsync -r "$radir/saves" "$my_date/"
-rsync -r "$radir/states" "$my_date/"
-rsync -r "$radir/retroarch.cfg" "$my_date/"
+do_backup () {
 
+	my_date=$(date +'%Y-%m-%d')
+	cd /media/pi/RA-DATA/rasp_pi_retroarch_conf_backups/
+	radir="/home/pi/snap/retroarch/427/.config/retroarch"
+	mkdir -p "$my_date/system/"
+	rsync -r "$radir/system" "$my_date/"
+	rsync -r "$radir/config" "$my_date/"
+	rsync -r "$radir/autoconfig" "$my_date/"
+	rsync -r "$radir/playlists" "$my_date/"
+	rsync -r "$radir/saves" "$my_date/"
+	rsync -r "$radir/states" "$my_date/"
+	rsync -r "$radir/retroarch.cfg" "$my_date/"
+}
 #retroarch -f && sudo shutdown now
-retroarch -f && echo 'To prevent shutdown, close this window!'; sleep 7;  sudo shutdown now
+retroarch -f && echo 'Doing RetroArch backup'; do_backup; echo 'Backup done'; echo 'To prevent shutdown, close this window!'; sleep 7;  sudo shutdown now
